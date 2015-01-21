835834c2a7ad94d3f7bff08708ef39b8736ccdf6
Paul Carleton <paulcarletonjr@gmail.com>
2015-01-20 22:41:36 -0800

    Fully functional

--- scraper.hs
+++ scraper.hs
@@ -1,37 +1,147 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy as L
 import Data.Tree.NTree.TypeDefs
 import Data.Maybe
 import Text.XML.HXT.Core
-import Network.HTTP.Conduit
+import qualified Network.HTTP.Conduit as C
+import Data.Char (chr)
+
+import Data.Map (Map)
+import qualified Data.Map as Map 
+
+import Data.List
+import Data.Maybe
+import qualified Data.Aeson as A
+import qualified Data.Text as T
+import GHC.Generics
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+data Link = Link
+    { source :: Int
+    , target  :: Int
+    } deriving (Show,Generic)
+    
+instance A.ToJSON Link
+
+data Node = Node
+    { name :: T.Text } deriving (Show,Generic)
+    
+instance A.ToJSON Node
+
+
+data Graph = Graph
+    {
+        nodes :: [Node],
+        links :: [Link]
+    } deriving (Show, Generic)
+instance A.ToJSON Graph
+
+type NL = (Set String, Set (String, String))
+type CMap = Map String [String]
+
+myUnpack :: L.ByteString -> String
+myUnpack = map (chr . fromEnum) . L.unpack
+
 
 fromFile :: String -> IO (IOSArrow XmlTree (NTree XNode))
 fromFile path = do
   contents <- readFile path
   return $ readString [withParseHTML yes, withWarnings no] contents
 
-type CMap = Map String [String]
+
+fromWeb :: String -> IO (IOSArrow XmlTree (NTree XNode))
+fromWeb uri = do
+    text <- C.simpleHttp uri
+    return $ readString [withParseHTML yes, withWarnings no] $ myUnpack text
 
 css :: ArrowXml a => String -> a XmlTree XmlTree
 css tag = multi (hasName tag)
 
-payments f = f >>> css "div" >>>  hasAttrValue "class" ((==) "paymentpage-subline") >>> getChildren >>> hasAttr "href" >>> getAttrValue "href"
+payments f = f >>> css "div" >>>  hasAttrValue "class" ("paymentpage-subline" ==)
+        >>> getChildren >>> hasAttr "href" >>> getAttrValue "href"
 
--- foldl (im getConns) (return mapStart) firstLinks
+friends f = f >>> css "div" >>>  hasAttrValue "class" ("friend-list" ==)
+        >>> getChildren >>> getChildren >>> hasAttr "href" >>> getAttrValue "href"
+
 
 getConns dest = do
     putStrLn $ "Getting :" ++ dest
     page <- fromWeb ("https://venmo.com" ++ dest)
     runX . payments $ page
-getConns "" = return []
 
-im :: (String -> IO [String]) -> IO CMap -> String -> IO CMap
-im f b a = do
-    bc <- b
-    if Map.member a bc then return bc else insertLinks f bc a
-    
+getConns "" = return []
 
 
 insertLinks :: (String -> IO [String]) -> CMap -> String -> IO CMap
+
 insertLinks f b a = do
     res <- f a
     return $ Map.insert a res b
+
+
+im :: String -> CMap -> IO CMap
+
+im a b = if Map.member a b then return b
+        else insertLinks getConns b a
+
+
+getWeb :: Int -> [String] -> CMap -> IO CMap
+
+getWeb mx [] b = return b
+getWeb mx (x:xs) b = --return b
+    let nm = im x b in
+        if length (Map.keys b) > mx then
+        do
+            nm <- im x b
+            -- ms <- (Map.findWithDefault [] x) =<< nm
+            getWeb mx (xs) nm
+        else 
+        do
+            nm <- im x b
+            -- ms <- (Map.findWithDefault [] x) =<< nm
+            getWeb mx (xs ++ Map.findWithDefault [] x nm) nm
+
+
+mergeNLs :: NL -> NL -> NL
+mergeNLs a b = (Set.union (fst a) (fst b), Set.union (snd a) (snd b))
+
+getNL :: String -> String -> NL
+getNL s t = if s == t then (Set.empty, Set.empty) else (Set.singleton t, Set.singleton (s, t))
+
+getNodeLinks :: (String, [String]) -> NL
+getNodeLinks sts = 
+    let
+        nls = map (getNL (fst sts)) (snd sts)
+    in foldl mergeNLs (Set.empty, Set.empty) nls
+
+decon :: CMap -> NL
+decon m = foldl mergeNLs (Set.empty, Set.empty) $ map getNodeLinks (Map.toList m)
+
+
+result <- getWeb 1 ["/Paul-Carleton"] Map.empty
+
+
+extractGraph :: CMap -> Graph
+extractGraph cm =
+    let dcd = decon cm
+        nodenames = Set.toList (fst dcd)
+        nodelist = map (Node . T.pack) $ Set.toList (fst dcd)
+        nodeID x = fromMaybe 0 $ elemIndex x nodenames
+        linklist = map (\x -> Link (nodeID (fst x)) (nodeID (snd x))) $ Set.toList (snd dcd)
+    in Graph nodelist linklist
+
+
+
+graphForUser :: String -> Int -> IO Graph
+
+graphForUser u deg = 
+    let result = getWeb deg ["/" ++ u] Map.empty
+    in fmap  extractGraph result
+
+
+
+
+

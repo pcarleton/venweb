eed3660f9c0dbd29426e58d4423283927d32db68
Paul Carleton <paul@judicata.com>
2015-10-11 22:21:48 -0700

    Fixes username validation issue.

--- static/scripts/mains/index.js
+++ static/scripts/mains/index.js
@@ -1,28 +1,77 @@
 require(
-        ["jquery",],
-        function($) {
+        ["jquery",
+         "text!templates/warning.html",
+         "text!templates/verifying.html",
+         "text!templates/search_default.html",
+        ],
+        function($, warnTemplate, verifyTemplate, searchDefaultTemplate) {
 
-        var goToGraph = function() {
-            var inputName = $("#username-box").val();
+        var userName = "";
+
+
+        var storeUserName = function() {
+            userName = $("#username-box").val();
 
             // Strip @ sign
-            if (inputName.indexOf("@") == 0) {
-                inputName = inputName.slice(1);
+            if (userName.indexOf("@") == 0) {
+                userName = userName.slice(1);
             }
+            userName = $.trim(userName);
+        };
 
-            if (inputName.length == 0) {
+        var goToGraph = function() {
+            window.location.href = "/graph/" + $.trim(userName);
+        };
+
+        var checkUsername = function() {
+            if (userName.length == 0) {
+                showError();
                 return;
             }
 
-            window.location.href = "/graph/" + $.trim(inputName);
+            $.get("/ping/"+userName, function(resp) {
+                if (resp == "present") {
+                    goToGraph();
+                } else {
+                    showError();
+                }
+            }).error(function() {
+                showError();
+            });
+        };
+
+        var validate = function() {
+            storeUserName();
+            $(".search-container").html(verifyTemplate);
+            checkUsername();
         }
 
-        // Hack to get click events on mobile
-        document.getElementById("submit-username").onclick = goToGraph;
+        var showError = function() {
+            $(".search-container").html(searchDefaultTemplate);
+            $("#username-box").val(userName);
 
-        $('#username-box').keyup(function(e){
-            if(e.keyCode == 13) {
-                goToGraph();
-            }
-        });
+            setupListeners();
+
+
+
+            $(".warning").html(warnTemplate);
+            $(".warning").toggle();
+        }
+
+        var hideWarning = function() {
+            $(".warning").slideUp();
+        }
+
+        var setupListeners = function() {
+            // Hack to get click events on mobile
+            document.getElementById("submit-username").onclick = validate;
+
+            $('#username-box').keyup(function(e){
+                if(e.keyCode == 13) {
+                    validate();
+                }
+            });
+        }
+
+        setupListeners();
 });
